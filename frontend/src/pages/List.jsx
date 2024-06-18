94c331ca06a38b13ae1dc40d0462675bb1343f6b
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-06-18 12:02:12 +0300

    making the first get request for the notes using await and fetch and CORS

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const NotesList = () => {
+
+  let [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    getNotes()
+  }
+  , [])
+
+  let getNotes = async () => { 
+    let response = await fetch('http://127.0.0.1:8000/api/notes/')
+    let data = await response.json()
+    setNotes(data)
+
+    console.log(data)
+  }
+
   return (
     <div>
       List
