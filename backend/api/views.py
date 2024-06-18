e404ff8da6f297855c8d0e8c73b487910e8fc866
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-06-18 23:11:33 +0300

    made the views RESTFUL

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Note
 from .serializers import NoteSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -42,15 +44,51 @@ def getRoutes(request):
     return Response(routes)
 
 
-@api_view(['GET'])
 def getNotes(request):
     notes = Note.objects.all()
+    if not notes:
+        return HttpResponse("No notes found", status=status.HTTP_404_NOT_FOUND)
     serializer = NoteSerializer(notes, many=True)
-    return Response(serializer.data)
-
+    return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def getNote(request, pk):
-    note = Note.objects.get(id=pk)
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExist:
+        return HttpResponse("Note not found", status=status.HTTP_404_NOT_FOUND)
     serializer = NoteSerializer(note, many=False)
-    return Response(serializer.data)
+    return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExist:
+        return HttpResponse("Note not found", status=status.HTTP_404_NOT_FOUND)
+    serializer = NoteSerializer(instance=note, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PATCH'])
+def updateNote(request, pk):
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExist:
+        return HttpResponse("Note not found", status=status.HTTP_404_NOT_FOUND)
+    serializer = NoteSerializer(instance=note, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExist:
+        return HttpResponse("Note not found", status=status.HTTP_404_NOT_FOUND)
+    note.delete()
+    return HttpResponse("Note was deleted", status=status.HTTP_204_NO_CONTENT)
